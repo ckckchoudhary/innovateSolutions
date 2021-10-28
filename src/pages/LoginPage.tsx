60c41aa746f36139014054a54ee3b06058be5071
Chetan Kumar <chetan.kumar@curefit.com>
2021-10-28 23:55:57 +0530

    enhancement: added small validation for login form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
-class LoginPage extends Component {
-    render() {
-        const login = () => {
+interface LoginPageState {
+    enteredEmail: string;
+    enteredPassword: string;
+}
+class LoginPage extends Component<any, LoginPageState>{
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            enteredEmail: "",
+            enteredPassword: ""
+        }
+
+    }
+
+    onEmailChange = (e: any) => {
+        this.setState({ enteredEmail: e?.target?.value });
+    }
+
+    onPwdChange = (e: any) => {
+        this.setState({ enteredPassword: e?.target?.value });
+    }
+
+    login = () => {
+        const { enteredEmail, enteredPassword } = this.state;
+        if (enteredEmail && enteredPassword) {
             localStorage.setItem("loggedIn", "true");
             window.location.href = "/"
+        }else{
+            alert("Please enter email and password!");
         }
+    }
+
+    render() {
+        const {enteredEmail, enteredPassword } = this.state;
         return (
-            <div style={{width: 300, background:"lightgrey", padding: 50, margin: 50}}>
+            <div style={{ width: 300, background: "lightgrey", padding: 50, margin: 50 }}>
                 Login Page
-                <div style={{display:"flex", justifyContent:"space-between", margin: 10}}>
+                <div style={{ display: "flex", justifyContent: "space-between", margin: 10 }}>
                     <label>Email</label>
-                    <input type="text" name="email" placeholder="Email" />
+                    <input type="text" name="email" placeholder="Email" onChange={this.onEmailChange} value={enteredEmail}/>
                 </div>
-                <div style={{display:"flex", justifyContent:"space-between", margin: 10}}>
+                <div style={{ display: "flex", justifyContent: "space-between", margin: 10 }}>
                     <label>password</label>
-                    <input type="password" name="password" />
+                    <input type="password" name="password" onChange={this.onPwdChange} value={enteredPassword} placeholder="password"/>
 
                 </div>
-                <button onClick={login}>Login</button>
+                <button onClick={this.login}>Login</button>
             </div>
         );
     }
